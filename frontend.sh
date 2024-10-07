4cf6e1d91be80cdbe869142c99173afa88108ad2
SRINU <vasu_anand01@yahoo.co.in>
2024-10-07 13:44:47 +0530

    v 1.0.2

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
-echo -e "\e[33 Installing Nginx \e[0"
+echo -e "\e[33m Installing Nginx \e[0m"
 dnf install nginx -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[33 Removing index in nginx \e[0"
+echo -e "\e[33m Removing index in nginx \e[0m"
 rm -rf /usr/share/nginx/html/* &>>"/tmp/roboshop.log"
 
-echo -e "\e[33 Downloading front end content \e[0"
+echo -e "\e[33m Downloading front end content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>"/tmp/roboshop.log"
 
-echo -e "\e[33 Extracting front end content \e[0"
+echo -e "\e[33m Extracting front end content \e[0m"
 cd /usr/share/nginx/html &>>"/tmp/roboshop.log"
 unzip /tmp/frontend.zip &>>"/tmp/roboshop.log"
 
-echo -e "\e[33 config the Roboshop file  \e[0"
+echo -e "\e[33m config the Roboshop file  \e[0m"
 cp /home/centos/shell-oct/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>"/tmp/roboshop.log"
 
 
-echo -e "\e[33 Enabling Nginx \e[0"
+echo -e "\e[33m Enabling Nginx \e[0m"
 systemctl enable nginx &>>"/tmp/roboshop.log"
 systemctl restart nginx &>>"/tmp/roboshop.log"
