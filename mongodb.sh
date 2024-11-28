e1c3d0ed401cfbc70e6ea2daff76c82c1f5c66bc
SRINU42 <srinu.karnam01@gmail.com>
2024-11-28 10:32:16 +0530

    0.0.2

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
 
-echo -e "\e[34 Config mongodb repo   \e[0"
+echo -e "\e[34m Config mongodb repo   \e[0m"
 cp /home/centos/shell-oct/mongo.repo /etc/yum.repos.d/mongo.repo &>>"/tmp/roboshop.log"
 
-echo -e "\e[34  Install MongoDb  \e[0"
+echo -e "\e[34m  Install MongoDb  \e[0m"
 dnf install mongodb-org -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[34  Enable mangodb  \e[0"
+echo -e "\e[34m  Enable mangodb  \e[0m"
 systemctl enable mongod &>>"/tmp/roboshop.log"
 systemctl restart mongod &>>"/tmp/roboshop.log"
 
