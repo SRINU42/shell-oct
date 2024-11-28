d6180df41cf4e577966312622f760633434c2982
SRINU42 <srinu.karnam01@gmail.com>
2024-11-28 10:19:59 +0530

    cataloge 0.0.1

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,40 @@
-echo -e "\e[31  Enable NodeJs 18  \e[0"
+echo -e "\e[36  Enable NodeJs 18  \e[0"
 dnf module disable nodejs -y &>>"/tmp/roboshop.log"
 dnf module enable nodejs:18 -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  install NodeJs   \e[0"
+echo -e "\e[36  install NodeJs   \e[0"
 dnf install nodejs -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  Adding roboshop user  \e[0"
+echo -e "\e[36  Adding roboshop user  \e[0"
 useradd roboshop &>>"/tmp/roboshop.log" &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  setup an app directory  \e[0"
+echo -e "\e[36  setup an app directory  \e[0"
 mkdir /app &>>"/tmp/roboshop.log" &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  Download the application code to created app directory  \e[0"
+echo -e "\e[36  Download the application code to created app directory  \e[0"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 created app directory  \e[0"
+echo -e "\e[36 created app directory  \e[0"
 cd /app &>>"/tmp/roboshop.log"
 unzip /tmp/catalogue.zip &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 download the dependencies   \e[0"
+echo -e "\e[36 download the dependencies   \e[0"
 cd /app 
 npm install &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 Setup SystemD Catalogue Service  \e[0"
+echo -e "\e[36 Setup SystemD Catalogue Service  \e[0"
 cp /home/centos/shell-oct/catalogue.service /etc/systemd/system/catalogue.service &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 Start the service \e[0"
+echo -e "\e[36 Start the service \e[0"
 systemctl daemon-reload &>>"/tmp/roboshop.log"
 systemctl enable catalogue &>>"/tmp/roboshop.log"
 systemctl start catalogue &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 Config mongodb repo \e[0"
+echo -e "\e[36 Config mongodb repo \e[0"
 cp /home/centos/shell-oct/mongo.repo /etc/yum.repos.d/mongo.repo &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 Install MongoDb \e[0"
+echo -e "\e[36 Install MongoDb \e[0"
 dnf install mongodb-org-shell -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[31 Load Master Data \e[0"
+echo -e "\e[36 Load Master Data \e[0"
 mongo --host 172.31.17.163 </app/schema/catalogue.js
--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
 
-echo -e "\e[31 Config mongodb repo   \e[0"
+echo -e "\e[34 Config mongodb repo   \e[0"
 cp /home/centos/shell-oct/mongo.repo /etc/yum.repos.d/mongo.repo &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  Install MongoDb  \e[0"
+echo -e "\e[34  Install MongoDb  \e[0"
 dnf install mongodb-org -y &>>"/tmp/roboshop.log"
 
-echo -e "\e[31  Enable mangodb  \e[0"
+echo -e "\e[34  Enable mangodb  \e[0"
 systemctl enable mongod &>>"/tmp/roboshop.log"
 systemctl restart mongod &>>"/tmp/roboshop.log"
 
